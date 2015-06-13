e44c027b69ccc551d95dcdbfc836bab44bb24ddd
Rahul Gaur <rahul.nbg@gmail.com>
2015-06-13 16:50:37 +0530

    cleaning up the module

--- address/views.py
+++ address/views.py
@@ -7,13 +7,13 @@ from .serializers import AddressSerializer
 from .serializers import AddressDetailSerializer
 
 class AddressList(generics.ListCreateAPIView):
-	queryset = Address.objects.all()
-        serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
 
 class AddressDetail(generics.RetrieveUpdateDestroyAPIView):
-        queryset = Address.objects.all()
-        serializer_class = AddressSerializer	
-   
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
 
 
-#curl -H 'Content-Type: application/json' -X PUT -d '{"address":"asdasdasd","latitude": 24,"longitude": 25}' http://127.0.0.1:8000/address/address/ 
+
+#curl -H 'Content-Type: application/json' -X PUT -d '{"address":"asdasdasd","latitude": 24,"longitude": 25}' http://127.0.0.1:8000/address/address/
