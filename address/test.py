fe04f641408d42a06f023890c0a138b1ace49b36
Rahul Gaur <rahul.nbg@gmail.com>
2015-06-14 18:28:17 +0530

    Fixed testcases

--- address/test.py
+++ address/test.py
@@ -5,34 +5,35 @@ from rest_framework.test import APITestCase
 
 class AddressTestCase(APITestCase):
 
+    url = '/address/address/'
+
+    data = {
+        "id": 1,
+        "address": "Some Random Address",
+        "long_position": "28.376804",
+        "lat_position": "78.640811",
+    }
+
+
     def test_create_address(self):
         """
         Ensure we can create a new address object.
         """
-        url = '/address/address/'
-        data = {
-            "id": 1,
-            "address": "Some Random Address",
-            "long_position": "28.376804",
-            "lat_position": "78.640811",
-        }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data, data)
-
+        self.assertEqual(response.data, self.data)
 
     def test_get_address(self):
-    	response = self.client.post(url, data, format='json')
-        request = self.client.get('/address/address/1/')
+        #response = self.client.post(self.url, self.data, format='json')
+        self.test_create_address()
+        request = self.client.get('/address/address/3/')
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-
 
     def test_get_address_list(self):
         request = self.client.get('/address/address/')
         self.assertEqual(request.status_code, status.HTTP_200_OK)
 
-
-	def test_delete_address(self):
+    def test_delete_address(self):
+        self.test_create_address()
         response = self.client.delete('/address/address/2')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
