8aaaa89c116bad71ccab28311e7eca816e455058
kushagra43 <kushagra43@gmail.com>
2015-06-14 18:21:17 +0530

    test case

--- address/test.py
+++ address/test.py
@@ -22,6 +22,19 @@ class AddressTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
 
-    def test_get_list(self):
+
+    def test_get_address(self):
+    	response = self.client.post(url, data, format='json')
+        request = self.client.get('/address/address/1/')
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+
+    def test_get_address_list(self):
         request = self.client.get('/address/address/')
         self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+
+	def test_delete_address(self):
+        response = self.client.delete('/address/address/2')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
